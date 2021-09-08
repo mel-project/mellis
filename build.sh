76861b1af19d9d3b5b6ff68786ea9f56e75bb82b
nullchinchilla <nullchinchilla@pm.me>
2021-09-08 12:43:43 -0500

    fix

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ python3 ./flatpak-cargo-generator.py ./ginkou-loader/Cargo.lock -o loader-cargo-
 python3 ./flatpak-cargo-generator.py ./melwalletd/Cargo.lock -o melwalletd-cargo-lock.json
 python3 ./flatpak-node-generator.py npm -r ./ginkou/package-lock.json -o npm-lock.json
 #flatpak-builder --force-clean --user --install output-build-dir org.themelio.Wallet.yml
-flatpak-builder --gpg-sign=nullchinchilla@pm.me --force-clean --repo=repo build-dir org.themelio.Wallet.yml
+flatpak-builder --install-deps-from flathub --gpg-sign=nullchinchilla@pm.me --force-clean --repo=repo build-dir org.themelio.Wallet.yml

6ee0310bcf2d257a1451f9dd1cbdcc40b5a24869
Floor Bored <shmendez@shhmini.local>
2021-11-22 13:39:53 -0600

    build script

--- mac/build-mac.sh
+++ mac/build-mac.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 
-PROJECT_ROOT=$( cd "$(dirname "$0")/.." ; pwd -P )
+
 MAC_ROOT=$( cd "$(dirname "$0")" ; pwd -P )
+PROJECT_ROOT=$( cd $MAC_ROOT/.. ; pwd -P )
 RES=$MAC_ROOT/ginkou.app/Contents/MacOS/res
-
-
 
 
 cd $MAC_ROOT
@@ -26,7 +25,15 @@ npm run smui-theme-light
 mv public $RES/ginkou-public
 
 cd $MAC_ROOT
+rm -rf dmg_setup
+mkdir dmg_setup
+mv ginkou.app dmg_setup
 
+cd dmg_setup
+ln -s /Applications
+
+cd $MAC_ROOT
+create-dmg $PROJECT_ROOT/builds/ginkou.dmg dmg_setup
 
 rm -rf $RES/tmp
 

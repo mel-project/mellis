0cd11bbceaf9c8475b26c6766007fbd849249b27
Shane Mendez <mendezsoftware@gmail.com>
2021-11-24 13:21:45 -0600

    working on the template

--- flatpak/build-local.sh
+++ flatpak/build-local.sh
@@ -15,8 +15,12 @@ elif [ -n "${NIXOS_FIRST_RUN}" ]; then
 fi
 
 
-GINKOU_BRANCH=$(git checkout `(cd ../ginkou && git log --format='%H' -n 1)`)
-WALLET_BRANCH=$(git checkout `(cd ../melwalletd && git log --format='%H' -n 1)`)
-LOADER_BRANCH=$(git checkout `(cd ../ginkou-loader && git log --format='%H' -n 1)`)
-e
-flatpak-builder build org.themelio.Wallet-dev.yml --force-clean --user --install
+export GINKOU_BRANCH=`(cd ../ginkou && git log --format='%H' -n 1)`
+export WALLET_BRANCH=`(cd ../melwalletd && git log --format='%H' -n 1)`
+export LOADER_BRANCH=`(cd ../ginkou-loader && git log --format='%H' -n 1)`
+export RELEASE=""
+export TARGET="debug"
+
+cat org.themelio.Wallet-dev-template.yml | envsubst '$GINKOU_BRANCH $WALLET_BRANCH $LOADER_BRANCH' > org.themelio.Wallet-dev.yml
+
+flatpak-builder org.themelio.Wallet-dev.yml --force-clean --user --install

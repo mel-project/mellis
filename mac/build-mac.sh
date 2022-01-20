637ad870ea3579b66e1ae4664d5391daf6939a8d
Shane Mendez <mendezsoftware@gmail.com>
2022-01-20 15:20:16 -0600

    running workflow

--- mac/build-mac.sh
+++ mac/build-mac.sh
@@ -14,7 +14,11 @@ rm -rf ginkou.app
 cp -r template.app ginkou.app
 mkdir -p $RES/tmp
 
-cargo build --path $PROJECT_ROOT/ginkou-loader/ 
+
+pushd $PROJECT_ROOT/ginkou-loader/ 
+    cargo build --locked
+popd 
+
 echo =======================
 # cargo install --locked --path $PROJECT_ROOT/melwalletd --root $RES/tmp
 mv $RES/tmp/bin/* $RES

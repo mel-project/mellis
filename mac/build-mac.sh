fc1bfac177aa3aa3297a68324f7b3550417af84f
Shane Mendez <mendezsoftware@gmail.com>
2022-01-21 13:49:58 -0600

    running workflow

--- mac/build-mac.sh
+++ mac/build-mac.sh
@@ -17,54 +17,52 @@ mkdir -p $RES/tmp
 
 echo "Starting Rust installation"
 
-cargo install --locked --path $PROJECT_ROOT/ginkou-loader --root $RES/tmp
+# cargo install --locked --path $PROJECT_ROOT/ginkou-loader --root $RES/tmp
 
-echo =======================
+# echo =======================
 
-cargo install --locked --path $PROJECT_ROOT/melwalletd --root $RES/tmp
+# cargo install --locked --path $PROJECT_ROOT/melwalletd --root $RES/tmp
 # mv $RES/tmp/bin/* $RES
-ls -la $RES
-ls -la $PROJECT_ROOT/ginkou-loader/target
-echo =======================
-exit 0
-echo "Building ginkou"
-if [[ ! -d $PROJECT_ROOT/ginkou-public ]]
-then 
-    cd $RES/tmp
-    git clone $PROJECT_ROOT/ginkou 
-    cd ginkou
-    npm install
-    npm run build
-    npm run smui-theme-light
-    mv public $PROJECT_ROOT/ginkou-public
-else 
-    echo "\`ginkou-public\` is available..."
-    echo "not rebuilding ginkou and using \`ginkou-public\` instead"
-fi
+# echo =======================
+# exit 0
+# echo "Building ginkou"
+# if [[ ! -d $PROJECT_ROOT/ginkou-public ]]
+# then 
+#     cd $RES/tmp
+#     git clone $PROJECT_ROOT/ginkou 
+#     cd ginkou
+#     npm install
+#     npm run build
+#     npm run smui-theme-light
+#     mv public $PROJECT_ROOT/ginkou-public
+# else 
+#     echo "\`ginkou-public\` is available..."
+#     echo "not rebuilding ginkou and using \`ginkou-public\` instead"
+# fi
 
-cp -r $PROJECT_ROOT/ginkou-public $RES/ginkou-public
+# cp -r $PROJECT_ROOT/ginkou-public $RES/ginkou-public
 
 
 
 
 # setup a directory with the ginkou app
-cd $MAC_ROOT
-rm -rf dmg_setup
-mkdir dmg_setup
-mv ginkou.app dmg_setup
+# cd $MAC_ROOT
+# rm -rf dmg_setup
+# mkdir dmg_setup
+# mv ginkou.app dmg_setup
 
 
-# add a sym link to applications users can drag ginkou into
-cd dmg_setup
-ln -s /Applications
+# # add a sym link to applications users can drag ginkou into
+# cd dmg_setup
+# ln -s /Applications
 
-# create the dmg
-cd $MAC_ROOT
-rm -rf ginkou.dmg
-create-dmg ginkou.dmg dmg_setup
+# # create the dmg
+# cd $MAC_ROOT
+# rm -rf ginkou.dmg
+# create-dmg ginkou.dmg dmg_setup
 
-# delete artifacts
-rm -rf dmg_setup
-rm -rf $RES/tmp
+# # delete artifacts
+# rm -rf dmg_setup
+# rm -rf $RES/tmp
 
 

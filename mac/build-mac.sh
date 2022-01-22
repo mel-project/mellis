801d047e530d80a4015c784bb788dc3dcebb2a84
Shane Mendez <mendezsoftware@gmail.com>
2022-01-22 16:49:08 -0600

    running workflow

--- mac/build-mac.sh
+++ mac/build-mac.sh
@@ -130,7 +130,7 @@ mkdir $TMP
 
 [[ -z $_GINKOU_LOADER ]] && install_ginkou_loader
 [[ -z $_MELWALLETD ]] && install_melwalletd
-[[ -z $_GINKOU ]] && install_ginkou
+[[ ! -z $_GINKOU ]] && install_ginkou
 rm -rf $TMP
 [[ -z $_APP ]] && build_app
 [[ -z $_DMG ]] && build_dmg

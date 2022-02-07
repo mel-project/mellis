ac1340cae74a82ca98ef154f0a04186d241c53d2
Shane Mendez <mendezsoftware@gmail.com>
2022-02-07 08:20:32 -0600

    updating flatpak build script and also removing flatpak from build to publish windows and mac releases

--- flatpak/build-flatpak.sh
+++ flatpak/build-flatpak.sh
@@ -1,28 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 
 # Before merge to master run `build-flatpak --manifest` and `build-flatpak --manifest --release`
 if [ -n "${ARCH_FIRST_RUN}" ]; then
   sudo pacman -S flatpak flatpak-builder elfutils patch
   sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-  sudo flatpak install -y flathub org.gnome.Sdk/x86_64/40 org.gnome.Platform/x86_64/40 flathub org.freedesktop.Sdk.Extension.rust-stable//20.08 flathub org.freedesktop.Sdk.Extension.node14//20.08
 elif [ -n "${DEBIAN_FIRST_RUN}" ]; then
   sudo apt update
   env DEBIAN_FRONTEND=noninteractive sudo apt -y install flatpak flatpak-builder libgtk-3-dev libwebkit2gtk-4.0-dev
   sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-  sudo flatpak install -y flathub org.gnome.Sdk/x86_64/40 org.gnome.Platform/x86_64/40 flathub org.freedesktop.Sdk.Extension.rust-stable//20.08 flathub org.freedesktop.Sdk.Extension.node14//20.08
 elif [ -n "${NIXOS_FIRST_RUN}" ]; then
   sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-  sudo flatpak install -y flathub org.gnome.Sdk/x86_64/40 org.gnome.Platform/x86_64/40 flathub org.freedesktop.Sdk.Extension.rust-stable//20.08 flathub org.freedesktop.Sdk.Extension.node14//20.08
 fi
 
+  sudo flatpak install -y flathub org.gnome.Sdk/x86_64/40 org.gnome.Platform/x86_64/40 flathub org.freedesktop.Sdk.Extension.rust-stable//21.08 flathub org.freedesktop.Sdk.Extension.node14//20.08
 
 export RELEASE_FLAG=""
 export USER_FLAG=""
 export TARGET="debug"
 
+ONLY_MANIFEST=0
 INSTALL_MANIFEST=""
 MANIFEST_FILE="org.themelio.Wallet-dev.yml"
+
+
+set -ex
 
 
 while [[ "$#" -gt 0 ]]; do
@@ -74,5 +76,5 @@ export LOADER_BRANCH=`(cd ../ginkou-loader && git log --format='%H' -n 1)`
 cat org.themelio.Wallet-dev-template.yml | 
 envsubst '$GINKOU_BRANCH $WALLET_BRANCH $LOADER_BRANCH $RELEASE_FLAG $TARGET' > $MANIFEST_FILE;
 
-
-(( $ONLY_MANIFEST < 0 )) && flatpak-builder build $MANIFEST_FILE --force-clean $INSTALL_MANIFEST --install-deps-from flathub $USER_FLAG
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+(( $ONLY_MANIFEST  < 1)) && flatpak-builder build $MANIFEST_FILE --force-clean $INSTALL_MANIFEST --install-deps-from flathub $USER_FLAG

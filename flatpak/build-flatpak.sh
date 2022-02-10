c2a2d948e7c54dbbcd890a0c126e20b114547900
Shane Mendez <mendezsoftware@gmail.com>
2022-02-10 14:41:54 -0600

    building manifest in gh action

--- flatpak/build-flatpak.sh
+++ flatpak/build-flatpak.sh
@@ -4,16 +4,10 @@
 # Before merge to master run `build-flatpak --manifest` and `build-flatpak --manifest --release`
 if [ -n "${ARCH_FIRST_RUN}" ]; then
   sudo pacman -S flatpak flatpak-builder elfutils patch
-  sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 elif [ -n "${DEBIAN_FIRST_RUN}" ]; then
   sudo apt update
   env DEBIAN_FRONTEND=noninteractive sudo apt -y install flatpak flatpak-builder libgtk-3-dev libwebkit2gtk-4.0-dev
-  sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-elif [ -n "${NIXOS_FIRST_RUN}" ]; then
-  sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-fi
 
-sudo flatpak install -y flathub org.gnome.Sdk/x86_64/40 org.gnome.Platform/x86_64/40 flathub org.freedesktop.Sdk.Extension.rust-stable//21.08 flathub org.freedesktop.Sdk.Extension.node14//20.08
 
 export RELEASE_FLAG=""
 export USER_FLAG=""

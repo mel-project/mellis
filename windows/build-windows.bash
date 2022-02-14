e88bd1d29bfc8329f17c83503b7b3c8b6e4fb455
Shane Mendez <mendezsoftware@gmail.com>
2022-02-14 16:24:55 -0600

    hopefully final windows change

--- windows/build-windows.bash
+++ windows/build-windows.bash
@@ -8,12 +8,11 @@ ISCC="./iscc/ISCC.exe"
 rm -rfv iscc
 mkdir -p iscc
 unzip IS6.zip -d iscc
-# cargo install --locked --path ../ginkou-loader
-# cargo install --locked --path ../melwalletd
+cargo install --locked --path ../ginkou-loader
+cargo install --locked --path ../melwalletd
 # (cd ../ginkou && npm ci && npm run build)
 
 mkdir -p artifacts
-touch artifacts/testing.txt
-# cp $(which melwalletd) artifacts
-# cp $(which ginkou-loader) artifacts
+cp $(which melwalletd) artifacts
+cp $(which ginkou-loader) artifacts
 sh -c "$ISCC setup.iss"

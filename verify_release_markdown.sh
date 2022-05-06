ce8fea63a50cc640c4c4b048f9186f38fb249995
Meade Kincke <thedarkula2049@gmail.com>
2022-05-06 11:29:32 -0700

    Added a script.

---
+++ verify_release_markdown.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check that the first line of CHANGELOG.md and LATEST_VERSION.md are the same
+
+
+CHANGELOG_VERSION=$(head -1 CHANGELOG.md | tr -d ['#',:blank:])
+
+LATEST_VERSION=$(head -1 LATEST_VERSION.md | tr -d ['#',:blank:])
+
+
+
+
+if [ $CHANGELOG_VERSION == $LATEST_VERSION ]; then
+  echo "Top entry for version in CHANGELOG.md and LATEST_VERSION.md match. Continuing."
+
+else
+  echo "Top entry for version in CHANGELOG.md and LATEST_VERSION.md do not match."
+  echo "Make sure that these files are up to date."
+  exit 1
+fi

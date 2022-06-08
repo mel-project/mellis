24306327304b79988ee0a0b7d147dc22d3891a57
Meade Kincke <thedarkula2049@gmail.com>
2022-06-08 14:17:30 -0700

    Updated a script.

--- mac/template.app/Contents/MacOS/mellis.sh
+++ mac/template.app/Contents/MacOS/mellis.sh
@@ -2,9 +2,14 @@
 
 ROOT_DIRECTORY="$(pwd $(dirname "$0"))"
 RESOURCE_DIRECTORY="${ROOT_DIRECTORY}/res"
-PATH=${RESOURCE_DIRECTORY}:${PATH}
+export PATH=${RESOURCE_DIRECTORY}:${PATH}
 
 if [ -n "${DEBUG}" ]; then
+  set -x
+
+  ls -la "${ROOT_DIRECTORY}"
+  ls -la "${RESOURCE_DIRECTORY}"
+
   which melwalletd
   which ginkou-loader
 fi

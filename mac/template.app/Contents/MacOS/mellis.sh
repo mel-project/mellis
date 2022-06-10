ca039df93a43260eef042105a701fba873896f0e
Meade Kincke <thedarkula2049@gmail.com>
2022-06-10 11:10:50 -0700

    Added more debugging.

--- mac/template.app/Contents/MacOS/mellis.sh
+++ mac/template.app/Contents/MacOS/mellis.sh
@@ -9,7 +9,14 @@ export DEBUG=true
 if [ -n "${DEBUG}" ]; then
   set -x
 
+  echo "Root directory is ${ROOT_DIRECTORY}
+
+  echo "Root directory contents:
   ls -la "${ROOT_DIRECTORY}"
+
+  echo "Resource directory is ${RESOURCE_DIRECTORY}
+
+  echo "Resource directory contents:
   ls -la "${RESOURCE_DIRECTORY}"
 
   which melwalletd
@@ -17,3 +24,6 @@ if [ -n "${DEBUG}" ]; then
 fi
 
 ginkou-loader --html-path ${RESOURCE_DIRECTORY}/ginkou-public
+
+
+read -p "Press Return to Close..."

1f00e27e036b936ed675efba3b7d5818c1498a1c
Meade Kincke <thedarkula2049@gmail.com>
2022-07-20 13:47:10 -0500

    Did some work on the macos script.

--- mac/template.app/Contents/MacOS/mellis.sh
+++ mac/template.app/Contents/MacOS/mellis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 2>&1 | tee /tmp/mellis.log # output to a log file and to stdout
@@ -7,18 +8,19 @@ ROOT_DIRECTORY=$( cd "$(dirname "$0")" ; pwd -P )
 RESOURCE_DIRECTORY="${ROOT_DIRECTORY}/res"
 
 export PATH=${RESOURCE_DIRECTORY}:${PATH}
-export DEBUG=${DEBUG:-0} # DEBUG with default value of 0
 
-if [ "${DEBUG}" -gt 0 ]; then
+if [ "${MELLIS_DEBUG}" != "false" ]; then
+  export MELLIS_DEBUG=true
+fi
+
+if [ -n "${MELLIS_DEBUG}" ]; then
   set -x
 
   echo "Root directory is ${ROOT_DIRECTORY}
-
   echo "Root directory contents:
   ls -la "${ROOT_DIRECTORY}"
 
   echo "Resource directory is ${RESOURCE_DIRECTORY}
-
   echo "Resource directory contents:
   ls -la "${RESOURCE_DIRECTORY}"
 
@@ -28,5 +30,6 @@ fi
 
 ginkou-loader --html-path "${RESOURCE_DIRECTORY}/ginkou-public" #wrapped in quotes to prevent issues when RESOURCE_DIRECTORY contains a space
 
-
- [ "$DEBUG" -gt 0 ] && read -p "Press Return to Close..."
+if [ -n "${MELLIS_DEBUG}" ]; then
+   && read -p "Press Return to Close..."
+fi
